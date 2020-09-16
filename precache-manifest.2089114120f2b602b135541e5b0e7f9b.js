6d6ac3b695e2b8238cf9b68244a58354b902787c
yesid <martinezcorreayesid@gmail.com>
2020-09-16 17:04:26 -0500

    Updates

---
+++ precache-manifest.2089114120f2b602b135541e5b0e7f9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f976ee77f1ff35ade514b3ab4e6aed",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "32b12ad2d74a24998c0a",
+    "url": "/react-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5567f761d182c6018d6",
+    "url": "/react-example/static/js/2.96b30471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.96b30471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b12ad2d74a24998c0a",
+    "url": "/react-example/static/js/main.a90ed067.chunk.js"
+  },
+  {
+    "revision": "591a35e8bba386688ec8",
+    "url": "/react-example/static/js/runtime-main.c33dc506.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
