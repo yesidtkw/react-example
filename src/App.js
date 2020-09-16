6b5216d4d6c089b13a6e859b459587fdf6e77083
yesid <martinezcorreayesid@gmail.com>
2020-09-16 17:08:51 -0500

    con cambios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React aplication - This is my aplication </h1>
+        <h1>Aplicacion de react  - Esta es mi aplicacion </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
